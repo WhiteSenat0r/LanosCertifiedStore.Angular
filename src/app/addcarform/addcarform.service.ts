6a828f40b2592bfcf67e83ba06ce7ab6489bde73
Nastya <92024271+havrylchyk@users.noreply.github.com>
2024-02-15 17:59:02 +0200

    change design

--- src/app/addcarform/addcarform.service.ts
+++ src/app/addcarform/addcarform.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Type } from '../shared/models/type';
 import { Brand } from '../shared/models/brand';
 import { Color } from '../shared/models/color';
 import { Model } from "../shared/models/model";
 import { Displacement } from "../shared/models/displacement";
+import { Vehicle } from "../shared/models/vehicle";
 
 @Injectable({
     providedIn: 'root'
@@ -35,4 +36,34 @@ export class AddcarformService {
         return this.http.get<Displacement[]>(this.baseUrl + 'Displacements');
     }
 
+    addBrand(newBrandName: string) {
+        const params = new HttpParams().set('name', newBrandName);
+        return this.http.post(this.baseUrl + 'Brands', null, { params });
+    }
+
+    addModel(newModelName: string, brandId: string) {
+        const params = new HttpParams().set('name', newModelName).set('brandId', brandId);
+        return this.http.post(this.baseUrl + 'Models', null, { params });
+    }
+
+    addType(newTypeName: string) {
+        const params = new HttpParams().set('name', newTypeName);
+        return this.http.post(this.baseUrl + 'Types', null, { params });
+    }
+
+    addDisplacement(newDisplacementValue: string) {
+        const params = new HttpParams().set('value', newDisplacementValue);
+        return this.http.post(this.baseUrl + 'Displacements', null, { params });
+    }
+
+    addColor(newColorName: string) {
+        const params = new HttpParams().set('name', newColorName);
+        return this.http.post(this.baseUrl + 'Colors', null, { params });
+    }
+      
+
+    addVehicle(newVehicle: Vehicle) {
+        return this.http.post(this.baseUrl + 'Vehicles', newVehicle);
+    }
+    
 }
