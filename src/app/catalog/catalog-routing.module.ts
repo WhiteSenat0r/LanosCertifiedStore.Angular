6f314b886fe717368456720ba51dc8cfb42b4e92
junarik <113181044+junarik@users.noreply.github.com>
2024-02-16 11:38:18 +0200

    Changed UI and fucntionality for pagination

--- src/app/catalog/catalog-routing.module.ts
+++ src/app/catalog/catalog-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {CatalogComponent} from "./catalog.component";
 import {CarDetailsComponent} from "./car-details/car-details.component";
