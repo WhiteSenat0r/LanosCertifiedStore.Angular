a8e9d3bc1a696700b4c9e3c2d597360a265c8017
Nastya <92024271+havrylchyk@users.noreply.github.com>
2024-03-06 21:04:20 +0200

    add star-tooltip

--- src/app/features/home/ratig/ratig.component.ts
+++ src/app/features/home/ratig/ratig.component.ts
@@ -6,9 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./ratig.component.css']
 })
 export class RatigComponent {
+
   rating: number = 0;
 
+  showFeedback: boolean = false;
+  hoverStarNumber: number = 0; 
+  isHovered: boolean = false; 
+
   rate(index: number): void {
+    this.showFeedback = true;
     if (this.rating === index) {
       this.rating = 0;
     } else {
@@ -18,5 +24,15 @@ export class RatigComponent {
 
   isRated(index: number): boolean {
     return index < this.rating;
+  }
+
+  showStarNumber(starNumber: number): void {
+    this.hoverStarNumber = starNumber;
+    this.isHovered = true;
+  }
+
+  hideStarNumber(): void {
+    this.hoverStarNumber = 0;
+    this.isHovered = false;
   }
 }
