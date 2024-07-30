01672a5cd2bcc172bf8413c4e5a342a181373279
Nastya <92024271+havrylchyk@users.noreply.github.com>
2024-07-30 18:46:25 +0300

    create modal window with filters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { NotFoundComponent } from './core/ui/not-found/not-found.component';
+import { MaincharacteristicsComponent } from './filters/maincharacteristics/maincharacteristics.component';
+import { TechnicalparametersComponent } from './filters/technicalparameters/technicalparameters.component';
 
 const routes: Routes = [
   {
     path: '',
     title: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'main',
+    title: 'main',
+    component: MaincharacteristicsComponent,
+  },
+  {
+    path: 'type',
+    title: 'type',
+    component: TechnicalparametersComponent,
   },
   { path: '**', component: NotFoundComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './features/home/home.module';
+import { MaincharacteristicsComponent } from './filters/maincharacteristics/maincharacteristics.component';
+import { TechnicalparametersComponent } from './filters/technicalparameters/technicalparameters.component';
+import { TransmissionComponent } from './filters/transmission/transmission.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MaincharacteristicsComponent,
+    TechnicalparametersComponent,
+    TransmissionComponent,
   ],
   imports: [
     BrowserModule,
