e2d18550b38fdf217bf4faf72f40a6740c78dec8
junarik <113181044+junarik@users.noreply.github.com>
2024-08-03 20:55:29 +0300

    carousel and routing and footer check

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeroSectionComponent } from './ui/hero-section/hero-section.component';
 import { MainContentAreaComponent } from './ui/main-content-area/main-content-area.component';
 import { BrandTilesComponent } from './ui/main-content-area/brand-tiles/brand-tiles.component';
+import { CarProposalsCardsComponent } from './ui/main-content-area/car-proposals-cards/car-proposals-cards.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 
 
@@ -12,13 +14,15 @@ import { BrandTilesComponent } from './ui/main-content-area/brand-tiles/brand-ti
     HomeComponent,
     HeroSectionComponent,
     MainContentAreaComponent,
-    BrandTilesComponent
+    BrandTilesComponent,
+    CarProposalsCardsComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HomeComponent
-  ]
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule { }
